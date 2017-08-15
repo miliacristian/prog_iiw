c8399a9c2e3a7ada3ae651573388fc23248dd470
cristian <miliacristian4@gmail.com>
2017-08-16 00:09:03 +0200

    sxzx

--- list_client.c
+++ list_client.c
@@ -178,6 +178,70 @@ int rcv_list(int sockfd,struct sockaddr_in serv_addr,socklen_t len,struct temp_b
         }
     }
 }
+int rcv_list2(int sockfd,struct sockaddr_in serv_addr,socklen_t len,struct temp_buffer temp_buff,struct window_snd_buf *win_buf_snd,struct window_rcv_buf *win_buf_rcv,int *seq_to_send,int W,int *pkt_fly,char*list,int dimension,double loss_prob,int *window_base_snd,int *window_base_rcv,int *byte_written){
+    start_timeout_timer(timeout_timer_id,TIMEOUT);
+    send_message_in_window_cli(sockfd,&serv_addr,len,temp_buff,win_buf_snd,"START",START,seq_to_send,loss_prob,W,pkt_fly);
+    printf("messaggio start inviato\n");
+    printf("rcv list\n");
+    errno=0;
+    while (1) {
+        if (recvfrom(sockfd, &temp_buff, sizeof(struct temp_buffer), 0, (struct sockaddr *) &serv_addr, &len) != -1) {//bloccati finquando non ricevi file
+            // o altri messaggi
+            if(temp_buff.command==SYN || temp_buff.command==SYN_ACK){
+                continue;//ignora pacchetto
+            }
+            else{
+                stop_timeout_timer(timeout_timer_id);
+            }
+            printf("pacchetto ricevuto con ack %d seq %d command %d\n", temp_buff.ack, temp_buff.seq, temp_buff.command);
+            if (temp_buff.seq == NOT_A_PKT && temp_buff.ack!=NOT_AN_ACK) {
+                if(seq_is_in_window(*window_base_snd, W, temp_buff.ack)){
+                    rcv_ack_in_window(temp_buff,win_buf_snd,W,window_base_snd,pkt_fly);
+                }
+                else{
+                    printf("ack duplicato\n");
+                }
+                start_timeout_timer(timeout_timer_id,TIMEOUT);
+            }
+            else if (!seq_is_in_window(*window_base_rcv, W,temp_buff.seq)) {
+                rcv_msg_re_send_ack_command_in_window(sockfd,&serv_addr,len,temp_buff,loss_prob);
+                start_timeout_timer(timeout_timer_id,TIMEOUT);
+            }
+            else if(seq_is_in_window(*window_base_rcv, W,temp_buff.seq)) {
+                if (temp_buff.command == DATA) {
+                    rcv_list_send_ack_in_window(sockfd, list, &serv_addr, len, temp_buff, win_buf_rcv, window_base_rcv,loss_prob, W, dimension, byte_written);
+                    if (*byte_written == dimension) {
+                        wait_for_fin_list(temp_buff, win_buf_snd, sockfd, serv_addr, len, window_base_snd,
+                                          window_base_rcv, pkt_fly, W, byte_written, loss_prob);
+                        return *byte_written;
+                    }
+
+                }
+                else{
+                    rcv_msg_send_ack_command_in_window(sockfd,&serv_addr,len,temp_buff,win_buf_rcv,window_base_rcv,loss_prob,W);
+                }
+                start_timeout_timer(timeout_timer_id, TIMEOUT);
+            }
+            else {
+                printf("ignorato pacchetto wait dimension con ack %d seq %d command %d\n", temp_buff.ack, temp_buff.seq,
+                       temp_buff.command);
+                printf("winbase snd %d winbase rcv %d",*window_base_snd,*window_base_rcv);
+                start_timeout_timer(timeout_timer_id,TIMEOUT);
+            }
+        }
+        else if(errno!=EINTR){
+            handle_error_with_exit("error in recvfrom\n");
+        }
+        if (great_alarm == 1) {
+            printf("il sender non sta mandando più nulla o errore interno\n");
+            great_alarm = 0;
+            stop_all_timers(win_buf_snd, W);
+            stop_timeout_timer(timeout_timer_id);
+            printf("return rcv list\n");
+            return *byte_written;
+        }
+    }
+}
 
 int wait_for_list_dimension(int sockfd, struct sockaddr_in serv_addr, socklen_t  len, int *byte_written , int *seq_to_send , int *window_base_snd , int *window_base_rcv, int W, int *pkt_fly , struct temp_buffer temp_buff ,struct window_rcv_buf *win_buf_rcv,struct window_snd_buf *win_buf_snd) {
     errno = 0;
@@ -220,12 +284,12 @@ int wait_for_list_dimension(int sockfd, struct sockaddr_in serv_addr, socklen_t 
                 if(list==NULL){
                   handle_error_with_exit("error in malloc\n");
                 }
-                memset(list,'\0',(size_t)dimension);
+                memset(list,'\0',dimension);
                 first=list;
-                rcv_list(sockfd, serv_addr, len, temp_buff, win_buf_snd, win_buf_rcv, seq_to_send, W, pkt_fly,list,
+                rcv_list2(sockfd, serv_addr, len, temp_buff, win_buf_snd, win_buf_rcv, seq_to_send, W, pkt_fly,list,
                          dimension, loss_prob, window_base_snd, window_base_rcv, byte_written);
                 printf("dimension %d\n",dimension);
-                printf("%d\n",strlen(first));
+                printf("%d\n",(int)strlen(first));
                 if(strlen(first)==(dimension-1)){
                     printf("list:\n%s",first);//stampa della lista ottenuta
                 }
